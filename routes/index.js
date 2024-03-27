6816b9cda34463464c1bfbd5dd7f3b7edca8cfca
trinh0909 <truongmaitrinh0909@gmail.com>
2024-03-27 22:15:10 +0700

    login

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/f', function(req, res) {
+  res.render('index');
+});
 module.exports = router;
